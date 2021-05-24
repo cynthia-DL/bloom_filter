2c1f92a93a474710daba0fdc83e1189f32360a5f
Guillaume <guillaumedomart@gmail.com>
2021-05-24 16:19:03 +0200

    [FIX] set_bitarray and print_bitarray [ADD] clear_bitarray

--- Main.c
+++ Main.c
@@ -1,10 +1,14 @@
 #include "filter.h"
 
 int main() {
-    int m = 72;
+    int m = 8;
     bitarray *a = create_bitarray(m);
-    set_bitarray(a, 67);
-    set_bitarray(a, 34);
+    set_bitarray(a, 8);
+    set_bitarray(a, 1);
+    set_bitarray(a, 3);
+    print_bitarray(a);
+    clear_bitarray(a);
+    printf("\n");
     print_bitarray(a);
     free_bitarray(a);
 
--- bitarray.c
+++ bitarray.c
@@ -1,8 +1,13 @@
 #include "bitarray.h"
 
+/**
+ * Create a bitarray and initialize it with w bits
+ * @param int m
+ * @return bitarray *
+ */
 bitarray *create_bitarray(int m){
     int i;
-    int taille = 1 + (m / 8) / (int) sizeof(int);
+    int taille = 1 + (m / 32);
 
     bitarray *bit_array = (bitarray *) malloc(sizeof(bitarray));
     bit_array->tab_bit = (int *) malloc(sizeof(int) * taille);
@@ -16,37 +21,52 @@ bitarray *create_bitarray(int m){
     return bit_array;
 }
 
+/**
+ * Free the memory associated with the bitarray a
+ * @param bitarray *a
+ */
 void free_bitarray(bitarray *a){
     free(a->tab_bit);
     free(a);
 }
 
+/** Set position pos in the bitarray to 1
+ * @param bitarray a
+ * @param int pos
+ */
 void set_bitarray(bitarray *a, int pos) {
+    if(pos > a->nb_bit)
+        exit(EXIT_FAILURE);
+
     int one = 1;
     int tab = pos / 32;
-    int bit = pos % 32 + tab;
+    int bit = (pos % 32) + 1;
 
     a->tab_bit[a->taille_tab - 1 - tab] = a->tab_bit[a->taille_tab - 1 - tab] | (one << (bit - 1));
     
 }
 
+/**
+ * Print the bitarray bit by bit
+ * @param bitarray *a
+ */
 void print_bitarray(bitarray *a) {
     int taille = a->taille_tab;
     int i, j;
     int octet;
+    int reste = a->nb_bit % 32;
 
     for (i = 0; i < taille; i++) {
         octet = a->tab_bit[i];
-        printf(" ");
-        for (j = 32; j >= 0; j--) {
-
-        
+        for (j = 32; j > 0; j--) {
             if ((octet / (int) pow(2, j)) == 1) {
                 octet = octet - pow(2, j);
-                printf("1");
+                if( (i != 0) || (j <= reste))
+                    printf("1");
             }
-            else{
-                printf("0");
+            else {
+                if( (i != 0) || (j <= reste))
+                    printf("0");
             }
         }
     }
@@ -54,9 +74,9 @@ void print_bitarray(bitarray *a) {
 
 /** 
  * Get the value at position pos in the bitarray
- * @param bittaray a
+ * @param bitarray *a
  * @param int pos
- * @return 0 or 1 depending of the value of the byte pos
+ * @return 0 or 1 depending of the value of the bit pos
  */
 int get_bitarray(bitarray* a, int pos){
     int tab;
@@ -76,7 +96,7 @@ int get_bitarray(bitarray* a, int pos){
 }
 
 /** Set position pos in the bitarray to 0 
- * @param bittaray a
+ * @param bitarray a
  * @param int pos
  */
 void reset_bitarray(bitarray *a, int pos){
@@ -90,4 +110,14 @@ void reset_bitarray(bitarray *a, int pos){
     bit = pos - 32 * ( a->taille_tab - tab - 1);
 
     a->tab_bit[tab] = (int) a->tab_bit[tab] & ~(1 << bit);
+}
+/**
+ * Put all bits to 0 int the bitarray a
+ * @param bitarray *a
+ */
+void clear_bitarray(bitarray *a){
+    int i;
+
+    for(i = 0; i < a->taille_tab; i++)
+        a->tab_bit[i] = 0;
 }
